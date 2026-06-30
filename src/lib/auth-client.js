740f2a19ac5ace58172df23a0bb5d4b58aa1f751
OmitHasanAdor <ibneshams05@gmail.com>
2026-07-01 03:55:59 +0600

    debugging patient dash 5

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,14 +1,14 @@
 import { createAuthClient } from "better-auth/react";
-import { jwtClient } from "better-auth/client/plugins";
+// import { jwtClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL:
     process.env.NEXT_PUBLIC_BETTER_AUTH_URL?.trim() ||
     "http://localhost:3000",
 
-  plugins: [
-    jwtClient()
-  ],
+  // plugins: [
+  //   jwtClient()
+  // ],
 });
 
 export const {

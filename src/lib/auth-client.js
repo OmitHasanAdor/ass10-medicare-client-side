dd21ba3f8f98aacd1b39796624e8415a6bd83ddb
OmitHasanAdor <ibneshams05@gmail.com>
2026-07-04 21:34:55 +0600

    updated auth client

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -11,9 +11,4 @@ export const authClient = createAuthClient({
   // ],
 });
 
-export const {
-  signIn,
-  signUp,
-  signOut,
-  useSession,
-} = authClient;
+export const { signIn, signUp, signOut, useSession } = createAuthClient()

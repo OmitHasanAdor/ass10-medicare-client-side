8a905d7f28b75fd292e53421c08cf5151deb2c4f
OmitHasanAdor <ibneshams05@gmail.com>
2026-08-01 00:44:31 +0600

    routing problem

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,16 +1,9 @@
 import { DashboardSidebar } from "@/components/DashboardSidebar";
-import { getUserSession } from "@/lib/core/session";
-import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({ children }) {
-    const user = await getUserSession();
-
-    if (!user) {
-        redirect("/auth/signin");
-    }
 
     return (
-        <div className="flex min-h-screen bg-zinc-50 dark:bg-zinc-950">
+        <div className="flex flex-col md:flex-row  min-h-screen bg-zinc-50 dark:bg-zinc-950">
             <DashboardSidebar />
           <div className="flex-1">{children}</div>
         </div>
